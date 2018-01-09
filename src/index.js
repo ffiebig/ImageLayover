260350065b2a3b195f734e0e99eb77c7ebe57472
Cristian Cortes <cacortes1@uc.cl>
2018-01-09 10:04:18 -0300

    avoid repetition in canvas creation

--- src/index.js
+++ src/index.js
@@ -1,69 +1,65 @@
-var images;
+import {createCanvas} from './create_canvas';
+
 var transparentImages;
 var imageArray;
+var canvases;
 
 function isTransparent(e) {
   var x = e.pageX - document.getElementById('image-tab-content').getBoundingClientRect().left + window.pageXOffset,
     y = e.pageY - document.getElementById('image-tab-content').getBoundingClientRect().top + window.pageYOffset,
-    transparent = true,
-    // detectedElement = null,
-    canvases = [];
-  var canvas;
-  var context2D;
-  var context;
+    transparent = true;
+  var context, image, canvas;
 
-  imageArray.forEach(function (image) {
+  canvases.forEach(function (tuple) {
+    image = tuple[0];
+    canvas = tuple[1];
+
+    if (window.getComputedStyle(image, null).getPropertyValue('display') === 'none') {
+      return false;
+    }
+
+    canvas.setAttribute('width', image.width);
+    canvas.setAttribute('height', image.height);
+    canvas.getContext('2d').drawImage(image, 0, 0, image.width, image.height);
 
     if (!transparent) {
       return true;
     }
 
-    canvas = (function (_this) {
-      e = document.createElement('canvas');
-      e.setAttribute('width', _this.width);
-      e.setAttribute('height', _this.height);
-      e.setAttribute('id', _this.id + '-canvas');
-      e.setAttribute('style', 'display: none;');
-      document.body.appendChild(e);
-      context2D = e.getContext('2d');
-      context2D.drawImage(_this, 0, 0, _this.width, _this.height);
-      return e;
-    })(image);
-
     if (canvas.getContext === undefined) {
       return false;
     }
 
-    canvases.push(canvas);
     context = canvas.getContext('2d');
-
-    transparent = (context.getImageData(x, y, 1, 1).data[3] === 0);
+    transparent = context.getImageData(x, y, 1, 1).data[3] === 0;
 
     if (transparent) {
-      // detectedElement = null;
       image.style.opacity = 1; // For real browsers;
       image.style.filter = 'alpha(opacity=100)'; // For IE;
     } else {
-      // detectedElement = image.id;
       image.style.opacity = 0.6;
       image.style.filter = 'alpha(opacity=60)';
     }
     return false;
   });
-  canvases.forEach(function (canvas) {
-    canvas.parentNode.removeChild(canvas);
-  });
+}
+
+export function removeListener(element) {
+  element.removeEventListener('mousemove', isTransparent);
+}
+
+export function startListener(element) {
+  element.addEventListener('mousemove', isTransparent);
 }
 
 (function () {
   var i;
 
-  images = document.getElementsByClassName('nn-image');
+  canvases = [];
   transparentImages = document.getElementsByClassName('nn-image-vacio');
-  imageArray = Array.from(images);
-  imageArray.reverse();
+  imageArray = Array.from(document.getElementsByClassName('nn-image'));
+  canvases = createCanvas(imageArray);
   for (i = 0; i < transparentImages.length; i++) {
-    transparentImages[i].addEventListener('mousemove', isTransparent);
-    // isTransparent(TransparentImages[i], ImageArray);
+    startListener(transparentImages[i]);
   }
 })();
